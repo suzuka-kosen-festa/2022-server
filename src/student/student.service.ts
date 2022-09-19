f9d0e2fde5f24f11d5d8301d651fb74f48a1acbf
Bayathy <misosoup0321@icloud.com>
2022-09-20 06:46:28 +0900

    update:student.serviceを書いた

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, Student } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(private readonly prisma : PrismaService){}
+
+  // Studentテーブルのレコードを作成する関数
+  async createStudent(data: Prisma.StudentCreateInput): Promise<Student> {
+    return this.prisma.student.create({
+      data
+    })
+  }
+
+  // Studentテーブルの全レコードを返す
+  async getStudents() : Promise<Student[]>{
+    return this.prisma.student.findMany()
+  }
+}
