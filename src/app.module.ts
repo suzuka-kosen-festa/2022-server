eada3e133ae0ccf32533fb564179f38153dd17ff
Bayathy <misosoup0321@icloud.com>
2022-09-20 06:34:53 +0900

    update:student.serviceの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { StudentService } from './student/student.service';
 
 @Module({
    imports: [],
    controllers: [AppController],
-   providers: [AppService, PrismaService],
+   providers: [AppService, PrismaService, StudentService],
 })
 export class AppModule {}
