e1d7bca046dd32e2cb1663013944282ffdb7e26f
Bayathy <misosoup0321@icloud.com>
2022-09-20 15:18:50 +0900

    clean:不要なインポートの削除

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { StudentService } from './student/student.service';
-import { StudentsController } from './students/students.controller';
 import { StudentController } from './student/student.controller';
 
 @Module({
    imports: [],
-   controllers: [AppController, StudentsController, StudentController],
+   controllers: [AppController, StudentController],
    providers: [AppService, PrismaService, StudentService],
 })
 export class AppModule {}
