e3a10b847a685f9d6e9dd086c35d05460a03898f
Bayathy <misosoup0321@icloud.com>
2022-09-25 19:25:29 +0900

    update:Prismaのシャットダウンの問題を解決する処理を書いた

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
@@ -12,6 +13,9 @@ async function bootstrap() {
       .build();
    const document = SwaggerModule.createDocument(app, config);
    SwaggerModule.setup('api', app, document);
+   
+   const prismaService = app.get(PrismaService);
+   await prismaService.enableShutdownHooks(app)
 
    await app.listen(7000);
 }
