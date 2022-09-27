93a4d175743af66a12341a6467aaf0bb197f4139
Bayathy <misosoup0321@icloud.com>
2022-09-27 21:53:28 +0900

    update:Healt.controllerの作成

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import {TerminusModule } from "@nestjs/terminus"
 import { PrismaHealthIndicator } from '../prisma/prisma.health';
+import { HealthController } from './health.controller';
 
 @Module({
   imports: [TerminusModule],
-  providers: [PrismaHealthIndicator]
+  providers: [PrismaHealthIndicator],
+  controllers: [HealthController]
 })
 export class HealthModule {}
