fa70d3e6299ce1c71e3c154b65fe7c0c665726d2
Bayathy <misosoup0321@icloud.com>
2022-09-27 21:51:44 +0900

    update:prisma.healthの定義

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import {TerminusModule } from "@nestjs/terminus"
+import { PrismaHealthIndicator } from '../prisma/prisma.health';
 
 @Module({
-  imports: [TerminusModule]
+  imports: [TerminusModule],
+  providers: [PrismaHealthIndicator]
 })
 export class HealthModule {}
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
+import { PrismaHealthIndicator } from './prisma.health';
 import { PrismaService } from './prisma.service';
 
 @Global()
 @Module({
-   providers: [PrismaService],
-   exports: [PrismaService],
+   providers: [PrismaService, PrismaHealthIndicator],
+   exports: [PrismaService, PrismaHealthIndicator],
 })
 export class PrismaModule {}
