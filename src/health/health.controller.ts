fe11928bf677b012ce5c89c1fd6bde3e51121a53
Bayathy <misosoup0321@icloud.com>
2022-09-27 22:26:30 +0900

    update:PrismaのHealthCheckの定義

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { PrismaHealthIndicator } from '../prisma/prisma.health';
 
 @Controller('health')
-export class HealthController {}
+export class HealthController {
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly prismaHealth : PrismaHealthIndicator
+  ){}
+
+  @Get()
+  @HealthCheck()
+  healthCheck(){
+    return this.health.check([
+      () => this.prismaHealth.isHealthy('db')
+    ])
+  }
+}
