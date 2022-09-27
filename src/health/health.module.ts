6a8e862392f418a20ce9f9b214a92904654da4da
Bayathy <Bayathy@users.noreply.github.com>
2022-09-27 19:38:41 +0000

    chore: コードを整形

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import {TerminusModule } from "@nestjs/terminus"
+import { TerminusModule } from '@nestjs/terminus';
 import { PrismaHealthIndicator } from '../prisma/prisma.health';
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [TerminusModule],
-  providers: [PrismaHealthIndicator],
-  controllers: [HealthController]
+   imports: [TerminusModule],
+   providers: [PrismaHealthIndicator],
+   controllers: [HealthController],
 })
 export class HealthModule {}
--- src/prisma/prisma.health.ts
+++ src/prisma/prisma.health.ts
@@ -1,19 +1,23 @@
-import { Injectable } from "@nestjs/common";
-import { HealthIndicator , HealthCheckError, HealthIndicatorResult } from "@nestjs/terminus";
-import { PrismaService } from "./prisma.service";
+import { Injectable } from '@nestjs/common';
+import {
+   HealthIndicator,
+   HealthCheckError,
+   HealthIndicatorResult,
+} from '@nestjs/terminus';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
-export class PrismaHealthIndicator extends HealthIndicator{
-  constructor(private readonly prismaService: PrismaService){
-    super()
-  }
+export class PrismaHealthIndicator extends HealthIndicator {
+   constructor(private readonly prismaService: PrismaService) {
+      super();
+   }
 
-  async isHealthy(key: string): Promise<HealthIndicatorResult> {
-    try {
-      await this.prismaService.$queryRaw`SELECT 1`;
-      return this.getStatus(key, true);
-    } catch (e) {
-      throw new HealthCheckError("Prisma check failed", e);
-    }
-  }
+   async isHealthy(key: string): Promise<HealthIndicatorResult> {
+      try {
+         await this.prismaService.$queryRaw`SELECT 1`;
+         return this.getStatus(key, true);
+      } catch (e) {
+         throw new HealthCheckError('Prisma check failed', e);
+      }
+   }
 }
