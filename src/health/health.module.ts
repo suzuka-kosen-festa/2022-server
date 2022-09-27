7fdb1784553ab08c2e74edec2acb9597d8b38f96
Bayathy <Bayathy@users.noreply.github.com>
2022-09-27 19:53:42 +0000

    chore: コードを整形

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import {TerminusModule } from "@nestjs/terminus"
+import { TerminusModule } from '@nestjs/terminus';
 import { PrismaModule } from '../prisma/prisma.module';
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [TerminusModule, PrismaModule],
-  controllers: [HealthController]
+   imports: [TerminusModule, PrismaModule],
+   controllers: [HealthController],
 })
 export class HealthModule {}
