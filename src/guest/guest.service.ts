7cdc5dd8fac1a14ff4a11b28c07cab9b3a2c7ac2
Bayathy <misosoup0321@icloud.com>
2022-09-30 03:58:58 +0900

    update:必要な関数を書いた

--- src/guest/guest.service.ts
+++ src/guest/guest.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Guest, Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class GuestService {}
+export class GuestService {
+  constructor( private readonly prisma : PrismaService){}
+
+  // ゲストは単独で存在してはいけないため(ホストとなるレコードが必要)、必要な関数はこれだけ
+
+  // Guestのデータの全件取得
+  async getAllGuests() : Promise<Guest[]> {
+    return this.prisma.guest.findMany()
+  }
+
+  //uuidの照合
+  async checkGuestExist(data : Prisma.GuestWhereUniqueInput): Promise<Guest>{
+    return this.prisma.guest.findUnique({
+      where: data
+    })
+  }
+}
