3ed1f0da2f8266fa858fe904da2101de08fb6359
Bayathy <misosoup0321@icloud.com>
2022-09-30 03:48:52 +0900

    add:Guestのmoduleの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { StudentModule } from './student/student.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { HealthModule } from './health/health.module';
+import { GuestModule } from './guest/guest.module';
 
 @Module({
-   imports: [StudentModule, PrismaModule, HealthModule],
+   imports: [StudentModule, PrismaModule, HealthModule, GuestModule],
 })
 export class AppModule {}
