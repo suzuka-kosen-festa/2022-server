0e6a3bae208650b671672535664939e5d2dad48a
Bayathy <misosoup0321@icloud.com>
2022-10-02 07:01:47 +0900

    update:JhsStudentServiceの定義

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,8 @@ import { StudentModule } from './student/student.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { HealthModule } from './health/health.module';
 import { GuestModule } from './guest/guest.module';
-import { JhsController } from './jhs/jhs.controller';
 import { JhsModule } from './jhs/jhs.module';
-
 @Module({
-   imports: [StudentModule, PrismaModule, HealthModule, GuestModule],
+   imports: [StudentModule, PrismaModule, HealthModule, GuestModule,JhsModule],
 })
 export class AppModule {}
--- src/jhs/jhs.service.ts
+++ src/jhs/jhs.service.ts
@@ -6,12 +6,31 @@ import { PrismaService } from '../prisma/prisma.service';
 export class JhsService {
   constructor(private readonly prisma : PrismaService){}
 
-  // async getAllJhs
+  async getAllJhs() : Promise<JHStudent[]>{
+    return this.prisma.jHStudent.findMany({
+      include:{
+        parents : true
+      }
+    })
+  }
   
+  async checkJhsExist(uuid :Prisma.JHStudentWhereUniqueInput): Promise<JHStudent>{
+    return this.prisma.jHStudent.findUnique({
+      where : uuid
+    })
+  }
+
   async createJhs(data : Prisma.JHStudentCreateInput): Promise<JHStudent> {
     return this.prisma.jHStudent.create({
       data
     })
   }
 
+  async updateJhs(params : {where:Prisma.JHStudentWhereUniqueInput, data: Prisma.JHStudentUpdateInput}) : Promise<JHStudent>{
+    const { where , data } = params
+    return this.prisma.jHStudent.update({
+      where : where,
+      data : data
+    })
+  }
 }
