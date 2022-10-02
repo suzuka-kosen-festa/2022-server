fad777b041a369e8fedae51c28db1cbdc14ba242
Bayathy <misosoup0321@icloud.com>
2022-10-02 21:29:56 +0900

    update:service書いた

--- src/ob/ob.service.ts
+++ src/ob/ob.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { OB, Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class ObService {}
+export class ObService {
+  constructor(private readonly prisma : PrismaService){}
+
+  async getall(): Promise<OB[]>{
+    return this.prisma.oB.findMany()
+  }
+
+  async create(data : Prisma.OBCreateInput): Promise<OB>{
+    return this.prisma.oB.create({data})
+  }
+
+  async chekUuid(uuid : Prisma.OBWhereUniqueInput): Promise<OB>{
+    return this.prisma.oB.findUnique({
+      where : uuid
+    })
+  }
+}
