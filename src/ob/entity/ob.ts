62c7db324b646161dd1925afe5deb3f07832a8c7
Bayathy <misosoup0321@icloud.com>
2022-10-03 01:12:01 +0900

    update:controllerの作成

--- src/ob/entity/ob.ts
+++ src/ob/entity/ob.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 
-export class Ob {
+export class ObEntity {
   @ApiProperty()
   obId :string
   @ApiProperty()
--- src/ob/ob.controller.ts
+++ src/ob/ob.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { OB } from '@prisma/client';
+import { CreateObDto } from './dto/ob-dto';
+import { ObEntity } from './entity/ob';
+import { ObService } from './ob.service';
 
 @Controller('ob')
-export class ObController {}
+export class ObController {
+  constructor(private readonly service : ObService){}
+
+  @Get()
+  @ApiOperation({summary: "OBのデータ全件取得"})
+  @ApiOkResponse({type : ObEntity , isArray: true})
+  async getAllOb() : Promise<OB[]>{
+    return this.service.getall()
+  }
+
+  @Get('check/:uuid')
+  @ApiOperation({summary:"OBのuuidの照合"})
+  @ApiOkResponse({type : ObEntity})
+  async checkExistOb(@Param('uuid') uuid : string): Promise<OB> {
+    return this.service.chekUuid({obId : uuid})
+  }
+
+  @Post()
+  @ApiOperation({summary:"OBレコードの作成"})
+  @ApiCreatedResponse({type: ObEntity})
+  async createOb(data : CreateObDto): Promise<OB>{
+    return this.service.create(data)
+  }
+
+}
