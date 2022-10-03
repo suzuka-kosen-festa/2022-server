c7ca724b6fbd896a71a39171577eccb67825900d
Bayathy <misosoup0321@icloud.com>
2022-10-03 17:49:11 +0900

    update:ob.serviceのテストを書いた

--- src/ob/ob.service.spec.ts
+++ src/ob/ob.service.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 import { ObService } from './ob.service';
+
+const obArray: Prisma.OBCreateInput[] = [
+   { obId: 'uuid1', name: 'てすと1', age: 25, email: 'test1@example.com' },
+   { obId: 'uuid2', name: 'てすと2', age: 26, email: 'test2@example.com' },
+   { obId: 'uuid3', name: 'てすと3', age: 27, email: 'test3@example.com' },
+   { obId: 'uuid4', name: 'てすと4', age: 28, email: 'test4@example.com' },
+];
+
+const singleRecord = obArray[0];
+
+const db = {
+   oB: {
+      findMany: jest.fn().mockResolvedValue(obArray),
+      findUnique: jest.fn().mockResolvedValue(singleRecord),
+      create: jest.fn().mockResolvedValue(singleRecord)
+   },
+};
+
 
 describe('ObService', () => {
    let service: ObService;
 
    beforeEach(async () => {
       const module: TestingModule = await Test.createTestingModule({
-         providers: [ObService],
+         providers: [ObService,
+         {provide:PrismaService,
+         useValue:db}],
       }).compile();
 
       service = module.get<ObService>(ObService);
@@ -15,4 +37,24 @@ describe('ObService', () => {
    it('should be defined', () => {
       expect(service).toBeDefined();
    });
+
+   it('create',async () => {
+      const obData = {
+         age : 25,
+         name : 'てすと１',
+         email: 'test1@example.com'
+      };
+      const createdOb = await service.create(obData);
+      expect(createdOb).toEqual(singleRecord);
+   })
+
+   it('getAll',async () => {
+      const obData = await service.getAll()
+      expect(obData).toStrictEqual(obArray)
+   })
+
+   it('checkExistOb',async () => {
+      const obRecord = await service.checkObExist({obId: 'uuid1'})
+      expect(obRecord).toStrictEqual(singleRecord)
+   })
 });
