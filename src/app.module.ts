a840b07da8aaeb559ef1637a363abba1c92b7cd8
Bayathy <misosoup0321@icloud.com>
2022-10-05 08:01:44 +0900

    chore:sponsor moduleの追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,8 @@ import { HealthModule } from './health/health.module';
 import { GuestModule } from './guest/guest.module';
 import { JhsModule } from './jhs/jhs.module';
 import { ObModule } from './ob/ob.module';
+import { SponsorModule } from './sponsor/sponsor.module';
 @Module({
-   imports: [StudentModule, PrismaModule, HealthModule, GuestModule, JhsModule, ObModule],
+   imports: [StudentModule, PrismaModule, HealthModule, GuestModule, JhsModule, ObModule, SponsorModule],
 })
 export class AppModule {}
