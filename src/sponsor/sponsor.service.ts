214e5d9255054269d9e88b030b2e1c38febf4048
Bayathy <misosoup0321@icloud.com>
2022-10-05 08:19:16 +0900

    update:serviceの定義

--- src/sponsor/sponsor.service.ts
+++ src/sponsor/sponsor.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, Sponsor } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class SponsorService {}
+export class SponsorService {
+  constructor(private readonly prisma : PrismaService){}
+
+  async getAllSponsor() : Promise<Sponsor[]>{
+    return this.prisma.sponsor.findMany()
+  }
+  
+  async createSponsor(data : Prisma.SponsorCreateInput) : Promise<Sponsor>{
+    return this.prisma.sponsor.create({data})
+  }
+
+  async deleteSponsor(wehere: Prisma.SponsorWhereUniqueInput) : Promise<Sponsor>{
+    return this.prisma.sponsor.delete({where: wehere})
+  }
+}
