78819a01f835386bcd8c65043f96858cb0787115
Bayathy <misosoup0321@icloud.com>
2022-10-05 21:40:06 +0900

    update:Sponsorのseviceを書いた

--- src/sponsor/entity/sponsor.ts
+++ src/sponsor/entity/sponsor.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 
-export class SponsorEntiry {
+export class SponsorEntity {
   @ApiProperty()
   sponsorId : string
   @ApiProperty()
--- src/sponsor/sponsor.controller.ts
+++ src/sponsor/sponsor.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { Sponsor } from '@prisma/client';
+import { createSponsorDto } from './dto/sponsorDto';
+import { SponsorEntity } from './entity/sponsor';
+import { SponsorService } from './sponsor.service';
 
 @Controller('sponsor')
-export class SponsorController {}
+export class SponsorController {
+  constructor(private readonly service : SponsorService){}
+
+  @Get()
+  @ApiOperation({summary:"Sponsorのレコードを全件返す"})
+  @ApiOkResponse({type: SponsorEntity})
+  async getAll() : Promise<Sponsor[]> {
+    return this.service.getAllSponsor()
+  }
+
+  @Post()
+  @ApiOperation({summary:"Sponsorレコードの生成"})
+  @ApiCreatedResponse({type: SponsorEntity})
+  async create(@Body() data : createSponsorDto) : Promise<Sponsor>{
+    return this.service.createSponsor(data)
+  }
+
+  @Delete(':uuid')
+  @ApiOperation({summary:"Sponsorレコードの削除"})
+  @ApiOkResponse({type: SponsorEntity})
+  async delete(@Param("uuid") uuid : string): Promise<Sponsor>{
+    return this.service.deleteSponsor({sponsorId: uuid})
+  }
+
+}
