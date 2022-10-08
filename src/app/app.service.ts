7ef13213afcf4ab5e5c198436254022ce1891232
Bayathy <misosoup0321@icloud.com>
2022-10-09 01:28:27 +0900

    update: appのserviceを書いた

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Guest, JHStudent, OB, Sponsor, Student } from '@prisma/client';
+import { GuestService } from '../guest/guest.service';
+import { JhsService } from '../jhs/jhs.service';
+import { ObService } from '../ob/ob.service';
+import { SponsorService } from '../sponsor/sponsor.service';
+import { StudentService } from '../student/student.service';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+    constructor(
+        private readonly studentService: StudentService,
+        private readonly guestServce : GuestService,
+        private readonly jhsService : JhsService,
+        private readonly obService : ObService,
+        private readonly sponsorService : SponsorService
+    ){}
+
+    parseUuid(reqUuid : string) : Promise<Student | OB | Guest | JHStudent | Sponsor>{
+        const [type, ...id]  = reqUuid
+        const uuid = id.join()
+
+        switch(type){
+            case "G":
+                return this.guestServce.checkGuestExist({guestId: uuid})
+            case "J":
+                return this.jhsService.checkJhsExist({jhsId:uuid})
+            case "O":
+                return this.obService.checkObExist({obId: uuid})
+            case "S":
+                return this.sponsorService.checkSponsorExist({sponsorId:uuid})
+            default:
+                throw new HttpException("uuid id not found", HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+}
