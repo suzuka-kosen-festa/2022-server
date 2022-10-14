391219f9a2cbd44ea6a4f44b95afa13e6ca5fff8
Bayathy <misosoup0321@icloud.com>
2022-10-14 21:20:44 +0900

    update: serviceとcontrollerの定義

--- src/history/history.controller.ts
+++ src/history/history.controller.ts
@@ -1,4 +1,13 @@
 import { Controller } from '@nestjs/common';
+// eslint-disable-next-line no-redeclare
+import { History } from '@prisma/client';
+import { HistoryService } from './history.service';
 
 @Controller('history')
-export class HistoryController {}
+export class HistoryController {
+  constructor(private readonly service : HistoryService){}
+
+ async getAllHistory() : Promise<History[]>{
+  return this.service.getAll()
+ }
+}
--- src/history/history.service.ts
+++ src/history/history.service.ts
@@ -1,4 +1,13 @@
+/* eslint-disable no-redeclare */
 import { Injectable } from '@nestjs/common';
+import { History } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class HistoryService {}
+export class HistoryService {
+  constructor(private readonly prisma : PrismaService){}
+
+  async getAll() : Promise<History[]>{
+    return this.prisma.history.findMany()
+  }
+}
