8e289534b03a02486c6b3f0d7217f43ad8b831db
Bayathy <misosoup0321@icloud.com>
2022-10-16 16:37:49 +0900

    refactor: Diのかきかたの修正

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
-import { GuestService } from '../guest/guest.service';
-import { JhsService } from '../jhs/jhs.service';
-import { ObService } from '../ob/ob.service';
-import { SponsorService } from '../sponsor/sponsor.service';
-import { StudentService } from '../student/student.service';
+import { GuestModule } from '../guest/guest.module';
+import { JhsModule } from '../jhs/jhs.module';
+import { ObModule } from '../ob/ob.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { SponsorModule } from '../sponsor/sponsor.module';
+import { StudentModule } from '../student/student.module';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
 @Module({
-   imports: [StudentsModule, GuestModule, JhsModule, ObModule, SponsorModule]
+   imports: [StudentModule, GuestModule, JhsModule, ObModule, SponsorModule, PrismaModule],
    providers: [AdminService],
    controllers: [AdminController],
 })
