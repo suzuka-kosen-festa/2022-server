acdd591fdf6f536db38be522cac233a102cac246
Bayathy <misosoup0321@icloud.com>
2022-10-20 01:32:21 +0900

    update: test書いた

--- src/bazaar/bazaar.service.spec.ts
+++ src/bazaar/bazaar.service.spec.ts
@@ -1,12 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 import { BazaarService } from './bazaar.service';
+
+const bazaarArray  =[
+   {id:1,name: "バザー1",descriptions:"説明1",image: "url1.com",group:"部活1",group_type:"eating", prices:[{price:"100円"}]},
+   {id:2,name: "バザー2",descriptions:"説明2",image: "url2.com",group:"部活2",group_type:"recreation", prices:[]},
+   {id:3,name: "バザー3",descriptions:"説明3",image: "url3.com",group:"部活3",group_type:"eating", prices:[]},
+   {id:4,name: "バザー4",descriptions:"説明4",image: "url4.com",group:"部活4",group_type:"recreation", prices:[]},
+   {id:5,name: "バザー5",descriptions:"説明5",image: "url5.com",group:"部活5",group_type:"eating", prices:[]},
+]
 
 describe('BazaarService', () => {
    let service: BazaarService;
 
-   beforeEach(async () => {
+   beforeAll(async () => {
       const module: TestingModule = await Test.createTestingModule({
-         providers: [BazaarService],
+         providers: [
+            BazaarService,
+            {
+               provide : PrismaService,
+               useValue : {
+                  bazaar:{
+                     findMany : jest.fn().mockResolvedValue(bazaarArray),
+                     findUnique : jest.fn().mockResolvedValue(bazaarArray[0]),
+                     create : jest.fn().mockResolvedValue(bazaarArray[0]),
+                     update: jest.fn().mockResolvedValue(bazaarArray[0]),
+                     delete : jest.fn().mockResolvedValue(bazaarArray[0])
+                  }
+               }
+            }
+         ],
       }).compile();
 
       service = module.get<BazaarService>(BazaarService);
@@ -15,4 +39,44 @@ describe('BazaarService', () => {
    it('should be defined', () => {
       expect(service).toBeDefined();
    });
+
+   it("getAll", async () =>{
+      const data = await service.getAll()
+
+      expect(data).toEqual(bazaarArray)
+   })
+
+   it("getByType", async () =>{
+      const data = await service.getByType({group_type : "eating"}) 
+
+      expect(data).toEqual(bazaarArray)
+   })
+
+   it("getById", async () =>{
+      const data = await service.getById({id: 1})
+
+      expect(data).toEqual(bazaarArray[0])
+   })
+
+   it("create", async () =>{
+      const bazaarData : Prisma.BazaarCreateInput = {
+         name: "バザー1",descriptions:"説明1",image: "url1.com",group:"部活1",group_type:"eating"
+      }
+      const data = await service.create(bazaarData)
+      expect(data).toEqual(bazaarArray[0])
+   })
+
+   it("update", async () =>{
+      const bazaarData : Prisma.BazaarUpdateInput = {
+         name: "バザー1",descriptions:"説明1",image: "url1.com",group:"部活1",group_type:"eating"
+      }
+      const data = await service.update({where:{id: 1}, data: bazaarData})
+
+      expect(data).toEqual(bazaarArray[0])
+   })
+
+   it("delete", async () =>{
+      const data = await service.delete({id : 1})
+      expect(data).toEqual(bazaarArray[0])
+   })
 });
