96cbdade7bee46e42ce4e177cb8dadf3d72c8d23
Bayathy <misosoup0321@icloud.com>
2022-10-22 02:46:58 +0900

    WIP

--- terraform/cloudruns.tf
+++ terraform/cloudruns.tf
@@ -64,3 +64,19 @@ resource "google_cloudbuild_worker_pool" "pool" {
     machine_type = "e2-standard-2"
   }
 }
+
+resource "google_cloudbuild_trigger" "include-build-logs-trigger" {
+  location = "us-west1"
+  name     = "include-build-logs-trigger"
+  filename = "cloudbuild.yaml"
+
+  github {
+    owner = "hashicorp"
+    name  = "terraform-provider-google-beta"
+    push {
+      branch = "^main$"
+    }
+  }
+
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+}
