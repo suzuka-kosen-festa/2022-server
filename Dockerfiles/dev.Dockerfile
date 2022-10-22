25b66d255b830106bd29c7b494828f399584fb70
Bayathy <misosoup0321@icloud.com>
2022-10-23 00:33:15 +0900

    update: terraformの設定をおおまかに書いた

--- Dockerfiles/dev.Dockerfile
+++ Dockerfiles/dev.Dockerfile
@@ -6,6 +6,10 @@ ENV NODE_ENV=development
 
 COPY package.json yarn.lock ./
 
+RUN yarn install --immutable
+
 COPY . .
 
 EXPOSE 7000
+
+CMD [ "./start.sh" ]
--- terraform/cloudruns.tf
+++ terraform/cloudruns.tf
@@ -1,3 +1,25 @@
+resource "google_sql_database_instance" "database" {
+  name             = "main-instance"
+  database_version = "POSTGRES_14"
+  region           = "us-west1"
+  root_password    = "root"
+
+  settings {
+    tier = "db-custom-1-3840"
+    disk_size = 10
+    disk_type = "PD_HDD"
+
+    backup_configuration {
+      enabled = true
+      start_time = "15:00"
+      point_in_time_recovery_enabled = true
+    }
+  }
+  # テスト用
+  deletion_protection = "false"
+}
+
+
 resource "google_cloud_run_service" "default" {
   name     = "server"
   location = "us-west1"
@@ -10,18 +32,47 @@ resource "google_cloud_run_service" "default" {
         # If true, garbage-collect CPU when once a request finishes
         # https://cloud.google.com/run/docs/configuring/cpu-allocation
         "run.googleapis.com/cpu-throttling" = false
+        "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.database.connection_name
+        "run.googleapis.com/client-name"        = "terraform"
       }
     }
 
     spec {
       containers {
-        image = "gcr.io/kosen-festa-server/server-image"
+        image = "us-west1-docker.pkg.dev/kosen-festa-server/my-repository/server-image"
         resources {
           limits = { "memory" : "0.5Gi", "cpu": "1" }
         }
 
         ports {
           container_port = "7000"
+        }
+
+        env {
+          name  = "DB_NAME"
+          value = google_sql_database.mydb.name
+        }
+        env {
+          name  = "DB_USER"
+          value = google_sql_user.users.name
+        }
+        env {
+          name  = "DB_PASS"
+          value = google_sql_user.users.password
+        }
+
+        env{
+          name = "INSTANCE_UNIX_SOCKET" 
+          value = "/cloudsql/kosen-festa-server:us-west1:main-instance"
+        }
+
+        env{
+          name = "INSTANCE_CONNECTION_NAME"
+          value = "kosen-festa-server:us-west1:main-instance"
+        }
+        env{
+          name = "DATABASE_URL"
+          value = "postgresql://user:password@localhost:5432/postgres?host=/cloudsql/kosen-festa-server:us-west1:main-instance&schema=public"
         }
       }
     }
@@ -39,28 +90,22 @@ resource "google_artifact_registry_repository" "my-repo" {
   format        = "DOCKER"
 }
 
-resource "google_sql_database_instance" "postgres" {
-  name             = "maindatabase"
-  database_version = "POSTGRES_14"
-  region           = "us-west1"
+resource "google_sql_database" "mydb" {
+  name     = "mydb"
+  instance = google_sql_database_instance.database.name
+}
 
-  settings {
-    tier = "db-custom-1-3840"
-    disk_size = 10
-    disk_type = "PD_HDD"
-
-    backup_configuration {
-      enabled = true
-      start_time = "15:00"
-      point_in_time_recovery_enabled = true
-    }
-  }
+resource "google_sql_user" "users" {
+  name     = "user"
+  password = "password"
+  instance = google_sql_database_instance.database.name
 }
 
 resource "google_cloudbuild_worker_pool" "pool" {
   name = "my-pool"
   location = "us-west1"
   worker_config {
+    disk_size_gb = 100
     machine_type = "e2-standard-2"
   }
 }
@@ -79,4 +124,21 @@ resource "google_cloudbuild_trigger" "include-build-logs-trigger" {
   }
 
   include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+ 
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.default.location
+  project  = google_cloud_run_service.default.project
+  service  = google_cloud_run_service.default.name
+ 
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
